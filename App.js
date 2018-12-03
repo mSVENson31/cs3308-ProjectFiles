755b7e2fe4636363773ee67b9da6759aa1e0e4f5
Dennis Lim <Dennis@cu-engr2-1-14-10.201.53.4.int.colorado.edu>
2018-12-03 16:56:39 -0700

    UI design for the search screen

--- App.js
+++ App.js
@@ -13,13 +13,13 @@ export default class App extends React.Component {
     super();
     this.state = {
       loading: true,
-      currentScreen: "login",
+      currentScreen: "search",
     };
   }
 
    // When the App component mounts, we listen for any authentication
    // state changes in Firebase.
-   // Once subscribed, the 'user' parameter will either be null 
+   // Once subscribed, the 'user' parameter will either be null
    // (logged out) or an Object (logged in)
   componentDidMount() {
     this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
@@ -62,7 +62,7 @@ export default class App extends React.Component {
     }
     else if (this.state.currentScreen === "sell" && this.state.user) {
       return (
-        <Sell 
+        <Sell
           switchScreen = {this.switchScreen}
           userEmail = {this.state.user.email}
           userId = {this.state.user.uid}
